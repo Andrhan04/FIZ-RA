0edde2cc2f5a37facec3c5eaa1d70926d32458db
Andrhan04 <andrham2004@yandex.ru>
2025-05-21 19:19:29 +0800

    complete

--- main.py
+++ main.py
@@ -10,13 +10,17 @@ path_to_save = ""
 #Сюда не лазить!!!!
 #-------------------------------------------------------------------------------------------------------------------------
 df_orders = pd.read_excel('Result.xlsx', index_col=0, sheet_name = name, header=0)
-data = ["Бег 100", "Кросс", "Прыжки в длинну", "Отжимания", "Подтягивания", "Наклоны", "Челночный бег", "Прес"]
+data : list = ["Бег 100", "Кросс", "Отжимания", "Подтягивания", "Наклоны", "Прес", "Бег 400", "Челночный бег", "Прыжки в длинну"]
+name_data = ["1 семестр","2 семестр","3 семестр","4 семестр","5 семестр","6 семестр"]
 
 def print_data(i : int):
-    print(i)
-    data_vav = [df_orders['Осень'][i], df_orders['Весна'][i]]
+    data_vav = []
+    for norm in name_data:
+        data_vav.append(df_orders[norm][i])
     fig, ax = plt.subplots()
-    ax.plot([1,2], data_vav, label = "Нормативы")
+    if(i == 8):
+        data_vav[0] = None
+    ax.plot(name_data, data_vav, label = "Нормативы", marker='o', markersize=3)
     ax.grid(True, linestyle='-.', linewidth=0.5, color='gray')
     ax.tick_params(axis='both', which='both', labelsize=8, width=1, color='red')
     plt.xlabel('Семестры') #Подпись для оси х
@@ -29,3 +33,5 @@ def print_data(i : int):
 
 for i in range(0,len(data)):
     print_data(i)
+    if(i >= 6):
+        print(f"Не все данные в {data[i]} норамтиве (не сдавали). И с этим боритесь как хотите!!!")
